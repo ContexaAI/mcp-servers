c226d63e9bb4c951b7ded502a79dd5c1658980e5
ajay.pawar <ajay.pawar@cyncly.com>
2025-07-23 01:12:17 +0530

    removed changes

--- supabase/packages/supabase/src/edge-function.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getDeploymentId(projectId: string, functionId: string, functionVersion: number): string;
-export declare function getPathPrefix(deploymentId: string): string;
-export declare const edgeFunctionExample: string;
-//# sourceMappingURL=edge-function.d.ts.map
